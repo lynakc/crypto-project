b68329b29fc33ea922b5f957f0735c4a536d6a96
Lyna <kacilyna00@gmail.com>
2026-05-03 21:57:09 +0100

    implementation MD5

--- asymetric/rsa.py
+++ asymetric/rsa.py
@@ -1,87 +1,33 @@
-import random
-
-def is_prime(n):
-  if n < 3:
-    return False
-  for i in range(2, int(n**0.5) + 1): #verifier de 2 a racine de n
-    if n % i == 0:
-      return False
-  return True
-
-def pgcd(a, b):
-  while b != 0:
-    a, b = b, a % b
-  return a
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 def get_key():
-  print("=== Génération des clés RSA ===")
+  try:
+    with open("private.pem", "rb") as f:
+      private_key = RSA.import_key(f.read())
+  except:
+    key = RSA.generate(1024)
+    private_key = key
+    with open("private.pem", "wb") as f:
+      f.write(private_key.export_key())
 
-  while True:
-    p = int(input("Entrer un nombre premier p: "))
-    if is_prime(p):
-      break
-    print("p n'est pas premier, veuiller réessayer")
+  public_key = private_key.publickey()
+  return public_key, private_key
 
-  while True:
-    q = int(input("Entrer un nombre premier q: "))
-    if is_prime(q):
-      break
-    print("q n'est pas premier, veuiller réessayer")
 
-  n = p * q
-  phi = (p - 1) * (q - 1)
+def encrypt(message, keys):
+  public_key, private_key = keys
 
-  while True:
-    e = int(input("Entrer e (premier avec phi): "))
-    if pgcd(e, phi) == 1:
-      break
-    print("e n'est pas premier avec phi(n), veuiller réessayer")
-  # calcul de d (inverse modulaire)
-  d = mod_inverse(e, phi)
+  cipher = PKCS1_OAEP.new(public_key)
+  ciphertext = cipher.encrypt(message.encode())
 
-  del p
-  del q
+  return ciphertext.hex()
 
-  #print("Clé publique (e, n):", (e, n))
-  #print("Clé privée (d, n):", (d, n))
 
-  return (e, d, n)
+def decrypt(ciphertext, keys):
+  public_key, private_key = keys
 
-def mod_inverse(e, phi):
-  def extended_pgcd(a, b):
-    if a==0:
-      return b, 0, 1
-    pgcd, x1, y1 = extended_pgcd(b%a, a)
-    x = y1 - (b//a)*x1
-    y = x1
-    return pgcd, x, y
-  
-  pgcd_val, x, y = extended_pgcd(e, phi)
-  if pgcd_val != 1:
-    return None
-  
-  return x%phi
+  cipher = PKCS1_OAEP.new(private_key)
+  decrypted = cipher.decrypt(bytes.fromhex(ciphertext))
 
-def encrypt(message, key):
-  e, d, n = key
-
-  result = []
-  for char in message: #boucle pour chaque caractere du message
-    c = pow(ord(char), e, n) #calculer x^e mod n
-    result.append(str(c))
-
-  return " ".join(result)
-  #return pow(int(message),e,n)
-
-def decrypt(message, key):
-  e, d, n = key
-
-  result = ""
-  numbers = message.split() #decouper la liste
-
-  for num in numbers:
-    m = pow(int(num), d, n)
-    result += chr(m)
-  
-  return result
-  #return pow(int(message),d,n)
+  return decrypted.decode()
